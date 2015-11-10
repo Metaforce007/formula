b0ae3514e8dba8facf807cf11ed76a39247f8601
Metaforce007 <Metaforce007@gmail.com>
2015-11-10 18:15:44 +0200

    Update print.h

--- source/print.h
+++ source/print.h
@@ -7,21 +7,25 @@ namespace fml {
 
     namespace mnl {
 
-        template <typename __I> void print(__I BG, __I EN, const char sep = '\n', std::ostream &OS = std::cout) {
+        template<typename __I>
+        void print(__I BG, __I EN, const char sep = '\n', std::ostream &OS = std::cout) {
             do OS << *BG << sep; while (++BG != EN);
         };
 
-        template <typename __I, typename __F> void print(__I BG, __I EN, const __F &F, const char sep = '\n', std::ostream &OS = std::cout) {
+        template<typename __I, typename __F>
+        void print(__I BG, __I EN, const __F &F, const char sep = '\n', std::ostream &OS = std::cout) {
             do OS << F(*BG) << sep; while (++BG != EN);
         };
 
     };
 
-    template <typename __C> void print(__C &C, const char sep = '\n', std::ostream &OS = std::cout) {
+    template<typename __C>
+    void print(__C &C, const char sep = '\n', std::ostream &OS = std::cout) {
         fml::mnl::print(C.begin(), C.end(), sep, OS);
     };
 
-    template <typename __C, typename __F> void print(__C &C, const __F &F, const char sep = '\n', std::ostream &OS = std::cout) {
+    template<typename __C, typename __F>
+    void print(__C &C, const __F &F, const char sep = '\n', std::ostream &OS = std::cout) {
         fml::mnl::print(C.begin(), C.end(), F, sep, OS);
     };
 
