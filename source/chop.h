24561205b2187b2a83ca5e4c3785dbae92d69547
Metaforce007 <Metaforce007@gmail.com>
2015-11-10 18:11:11 +0200

    Update chop.h

--- source/chop.h
+++ source/chop.h
@@ -6,7 +6,34 @@
 namespace fml {
 
     namespace mnl {
-    
+
+        ////////////////
+        // CLASSIFIED //
+        ////////////////
+
+        template<typename __I, typename __T>
+        void sub_chop(__I BI, __I EI, const __T &T, int &replaced);
+
+        template<typename __I>
+        int chop(__I BI, __I EI, int beg, int end);
+
+        /*template <typename __C> void __pop__(__C &C, int N) {
+            while (N--) C.pop_back();
+        };*/
+
+        /* C++ < 11
+        # include <string>
+        void __pop__(std::string &C, int N) {
+            std::string nC = ""; int rt = C.size() - N;
+            for (int i = 0; i < rt; i++) nC += C[i];
+            C = nC;
+        };*/
+
+    };
+
+    template<typename __C>
+    void chop(__C &C, int beg, int end) {
+        fml::mnl::__pop__(C, fml::mnl::chop(C.begin(), C.end(), beg, end));
     };
 
 };
