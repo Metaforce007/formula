2c406ebb4cde4d13e6d0e49d5237a91d1906b6ef
Metaforce007 <Metaforce007@gmail.com>
2015-11-10 14:24:32 +0200

    Create formula_chop.h

---
+++ source/formula_chop.h
@@ -0,0 +1,18 @@
+# ifndef    FORMULA_CHOP__GUARD
+# define    FORMULA_CHOP__GUARD
+
+# include   "pop.h"
+
+namespace fml {
+
+    namespace mnl {
+    
+    	// CLASSIFIED //
+
+    };
+    
+    // CLASSIFIED //
+
+};
+
+# endif
